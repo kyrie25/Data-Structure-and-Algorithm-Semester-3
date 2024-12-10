cd9871451f34bb3eccf348a1c876348e996d9bc2
JakeCoop <87562440+coopjake@users.noreply.github.com>
2024-12-10 22:15:16 +0700

    heap sort function declaration

--- Source File/header.h
+++ Source File/header.h
@@ -5,3 +5,7 @@
 //merge sort
 void merge(std::vector<int>& a, int left, int mid, int right);
 void mergeSort(std::vector<int>& a, int left, int right);
+
+//heap sort
+void heapify(std::vector<int>& a, int n, int i);
+void heapSort(std::vector<int>& a, int n);

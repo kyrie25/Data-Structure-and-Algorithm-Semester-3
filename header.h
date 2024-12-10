787ec32b99e41258c541b4f5456250dcb57ff053
JakeCoop <87562440+coopjake@users.noreply.github.com>
2024-12-10 21:37:05 +0700

    header file

---
+++ header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+//merge sort
+std::vector<int> merge (std::vector<int> a, std::vector<int> b, int n, int m);
+void mergeSort(std::vector<int>& a, int n);

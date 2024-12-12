324d26c22b68ce1e608246b7196438a4a351fd1e
JakeCoop <87562440+coopjake@users.noreply.github.com>
2024-12-12 16:00:37 +0700

    add Command5

--- src/command/command.h
+++ src/command/command.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <string>
 
-//void Command4(std::string algorithm1, std::string algorithm2, std::string inputFile);
+void Command4(std::string algorithm1, std::string algorithm2, std::string inputFile);
+void Command5(std::string algorithm1, std::string algorithm2, std::string inputFile, std::string inputOrder);

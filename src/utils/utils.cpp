a0b6f741f0333114796a1541bcfb60001a742a09
Nam Anh <phamnamanh25@gmail.com>
2024-12-12 04:31:45 +0000

    refactor: use vectors

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -11,65 +11,69 @@ void HoanVi(T &a, T &b)
 //-------------------------------------------------
 
 // Hàm phát sinh mảng dữ liệu ngẫu nhiên
-void GenerateRandomData(int a[], int n)
+void GenerateRandomData(vector<int> &arr)
 {
+	int n = arr.size();
 	srand((unsigned int)time(NULL));
 
 	for (int i = 0; i < n; i++)
 	{
-		a[i] = rand()%n;
+		arr.push_back(rand()%n);
 	}
 }
 
 // Hàm phát sinh mảng dữ liệu có thứ tự tăng dần
-void GenerateSortedData(int a[], int n)
+void GenerateSortedData(vector<int> &arr)
 {
+	int n = arr.size();
 	for (int i = 0; i < n; i++)
 	{
-		a[i] = i;
+		arr.push_back(i);
 	}
 }
 
 // Hàm phát sinh mảng dữ liệu có thứ tự ngược (giảm dần)
-void GenerateReverseData(int a[], int n)
+void GenerateReverseData(vector<int> &arr)
 {
-	for (int i = 0; i < n; i++)
+	int n = arr.size();
+	for (int i = n; i > 0; i--)
 	{
-		a[i] = n - 1 - i;
+		arr.push_back(i);
 	}
 }
 
 // Hàm phát sinh mảng dữ liệu gần như có thứ tự
-void GenerateNearlySortedData(int a[], int n)
+void GenerateNearlySortedData(vector<int> &arr)
 {
+	int n = arr.size();
 	for (int i = 0; i < n; i++)
 	{
-		a[i] = i;
+		arr.push_back(i);
 	}
 	srand((unsigned int) time(NULL));
 	for (int i = 0; i < 10; i ++)
 	{
 		int r1 = rand()%n;
 		int r2 = rand()%n;
-		HoanVi(a[r1], a[r2]);
+		HoanVi(arr[r1], arr[r2]);
 	}
 }
 
-void GenerateData(int a[], int n, int dataType)
+void GenerateData(vector<int> &arr, int dataType)
 {
 	switch (dataType)
 	{
 	case 0:	// ngẫu nhiên
-		GenerateRandomData(a, n);
+		GenerateRandomData(arr);
 		break;
 	case 1:	// có thứ tự
-		GenerateSortedData(a, n);
+		GenerateSortedData(arr);
 		break;
 	case 2:	// có thứ tự ngược
-		GenerateReverseData(a, n);
+		GenerateReverseData(arr);
 		break;
 	case 3:	// gần như có thứ tự
-		GenerateNearlySortedData(a, n);
+		GenerateNearlySortedData(arr);
 		break;
 	default:
 		printf("Error: unknown data type!\n");
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <vector>
 #include <time.h>
+
 using namespace std;
 
-template <class T>
-void HoanVi(T &a, T &b);
-void GenerateRandomData(int a[], int n);
-void GenerateSortedData(int a[], int n);
-void GenerateReverseData(int a[], int n);
-void GenerateNearlySortedData(int a[], int n);
-void GenerateData(int a[], int n, int dataType);
+void GenerateData(vector<int> &arr, int dataType);

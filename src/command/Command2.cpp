65183964255f3441fd851a695b4548a2b6437660
dtdat1234 <dotiendat1725@gmail.com>
2024-12-12 14:54:20 +0700

    add command 2

---
+++ src/command/Command2.cpp
@@ -0,0 +1,10 @@
+#include "include.h"
+
+void Command2(std::string algorithm, int input_size, std::string input_order, std::string output_param)
+{
+    std::cout << "ALGORITHM MODE\n"
+              << "Algorithm: " << algorithm << '\n'
+              << "Input size: " << input_size << '\n'
+              << "Input order: " << input_order << '\n'
+              << "------------------------------------\n";
+}

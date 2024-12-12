4fc107627e31dc142b600439f99428826e4d5816
JakeCoop <87562440+coopjake@users.noreply.github.com>
2024-12-12 19:21:41 +0700

    count comparison

--- src/algorithm/MergeSort.cpp
+++ src/algorithm/MergeSort.cpp
@@ -1,51 +1,58 @@
 #include "algorithm.h"
 
-void merge(std::vector<int> &arr, int left, int mid, int right) 
-{
+void merge(std::vector<int> &arr, int left, int mid, int right, int& countComparison) {
     int n = mid - left + 1;
     int m = right - mid;
 
     std::vector<int> b(n), c(m);
-    
-    for (int i = 0; i < n; i++) 
+
+    for (int i = 0; i < n; i++) {
         b[i] = arr[left + i];
-    for (int i = 0; i < m; i++)
+        countComparison++; 
+    }
+
+    for (int i = 0; i < m; i++) {
         c[i] = arr[mid + i + 1];
+        countComparison++; 
+    }
 
     int i = 0, j = 0, k = left;
 
-    while (i < n && j < m) 
-    {
-        if (b[i] < c[j])
+    while (i < n && j < m) {
+        countComparison += 3; 
+        if (b[i] < c[j]) {
             arr[k++] = b[i++];
-        else
+        } else {
             arr[k++] = c[j++];
+        }
     }
 
-    while (i < n) 
+    while (i < n) {
+        countComparison++; 
         arr[k++] = b[i++];
+    }
 
-    while (j < m)
-        arr[k++] = c[j++]; 
+    while (j < m) {
+        countComparison++; 
+        arr[k++] = c[j++];
+    }
 }
 
-void mergeSortRecur(std::vector<int> &arr, int left, int right) 
-{
-    if (left < right) 
-    {
+void mergeSortRecur(std::vector<int>& arr, int left, int right, int& countComparison) {
+    if (left < right) {
+        countComparison++; 
         int mid = (left + right) / 2;
 
-        mergeSortRecur(arr, left, mid);
-        mergeSortRecur(arr, mid + 1, right);
+        mergeSortRecur(arr, left, mid, countComparison);
+        mergeSortRecur(arr, mid + 1, right, countComparison);
 
-        merge(arr, left, mid, right);
+        merge(arr, left, mid, right, countComparison);
     }
 }
 
-void mergeSort(std::vector<int> &arr)
-{
+void mergeSort(std::vector<int>& arr, int& countComparison) {
     int left = 0;
-    int right = arr.size();
+    int right = arr.size() - 1;
 
-    mergeSortRecur(arr, left, right);
+    mergeSortRecur(arr, left, right, countComparison);
 }

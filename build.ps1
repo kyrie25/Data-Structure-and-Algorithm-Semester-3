18e7e59c22036d0df328343e87ef21c12c6b29c6
Nam Anh <phamnamanh25@gmail.com>
2024-12-14 08:26:50 +0700

    feat: use filename from build task

--- build.ps1
+++ build.ps1
@@ -1,2 +1,8 @@
+# Get --output file name
+$Output = $args[0]
+if ($Output -eq $null) {
+    $Output = "sort.exe"
+}
+
 $files = Get-ChildItem -Recurse -Filter *.cpp -Path .\src | ForEach-Object { $_.FullName }
-g++ '-Wl,--stack,1000000000' -fdiagnostics-color=always -g main.cpp $files -o main.exe
+g++ '-Wl,--stack,1000000000' -fdiagnostics-color=always -g main.cpp $files -o $Output

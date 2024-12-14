54273819968d97f29691135e02c2906537b8addb
DENVUI <159744487+DENVUI@users.noreply.github.com>
2024-12-14 21:07:55 +0700

    fix countComparison

--- src/algorithm/InsertionSort.cpp
+++ src/algorithm/InsertionSort.cpp
@@ -3,16 +3,14 @@
 void insertionSort(std::vector<int> &arr, int &countComparison)
 {
     int n = arr.size();
-
-    for (int i = 1; i < n; ++i) {
+    countComparison=0;
+    for (int i = 1; ++countComparison && i < n; ++i) {
         int key = arr[i];
         int j = i - 1;
 
-        /* Move elements of arr[0..i-1], that are
-           greater than key, to one position ahead
-           of their current position */
-        while (j >= 0 && arr[j] > key) {
-            countComparison += 2;
+        
+        while (  j >= 0 && arr[j] > key) {
+           countComparison+=2
             arr[j + 1] = arr[j];
             j = j - 1;
         }

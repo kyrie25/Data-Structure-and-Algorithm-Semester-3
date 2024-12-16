fa0d4f952a81035af1cc272276ecdbd49d4c0782
thich-hoc-lap-trinh <sungyin1112@gmail.com>
2024-12-16 13:38:09 +0000

    test

--- src/command/Command2.cpp
+++ src/command/Command2.cpp
@@ -50,7 +50,7 @@ void Command2(const std::string &algorithm, const int &input_size,
     std::cout << "Running time (if required): ";
     if (outputParam == TIME || outputParam == BOTH)
     {
-        std::cout << runTime.count() << " ms" << '\n';
+        std::cout << setprecision(20) << fixed << runTime.count() << " ms" << '\n';
     }
     else
     {
